fa7d89b796c4ca0283e3de0266551e7cc0083ba3
Abir Mahmud <abir2mahmud@gmail.com>
2026-01-22 16:24:11 +0600

    implement some features

---
+++ app/Components/ResumeCard.tsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router";
+
+const ResumeCard = ({ resume }: { resume: Resume}) => {
+    return (
+        <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
+            <div className="flex flex-col gap-2">
+
+            </div>
+        </Link>
+    )
+}
+export default ResumeCard
